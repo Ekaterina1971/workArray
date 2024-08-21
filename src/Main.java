95e6ca260bd01f0ed397ef600b3764aa23b4314c
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-21 11:53:53 +0300

    задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача №1
+        System.out.println("Задача №1");
+        int [] apples = new int [3];
+        apples [0] = 1;
+        apples [1] = 2;
+        apples [2] = 3;
+        System.out.println(Arrays.toString(apples));
+        double [] fraction = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(fraction));
+        int [] letters = new int [3];
+        letters [0] = 1;
+        letters [1] = 17;
+        letters [2] = 5;
+        System.out.println(Arrays.toString(letters));
     }
 }
